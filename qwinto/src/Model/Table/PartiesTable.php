8137b0f929f84d81cd4ea8d8851e11c4ea586a4e
PetersBrice <brice.peters@laposte.net>
2015-11-13 16:01:49 +0100

    mise en place de la table outil

--- qwinto/src/Model/Table/PartiesTable.php
+++ qwinto/src/Model/Table/PartiesTable.php
@@ -6,7 +6,21 @@ use Cake\ORM\Table;
 class PartiesTable extends Table
 {
     var $tour;
+    
     public function initialize(array $config)
     {
     }
+    
+    public function fin(){
+     return false;   
+    }
+    
+    public function gagnant(){
+        return 0;
+    }
+    
+    public function ajoutJoueur(Utilisateur utilisateur){
+        
+    }
+    
 }
