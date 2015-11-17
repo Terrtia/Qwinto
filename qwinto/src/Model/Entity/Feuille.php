b4c514c939cd37162fd42fb2b698473950debe47
DecolleAnais <anais.decolle8@etu.univ-lorraine.fr>
2015-11-17 16:45:52 +0100

    ajout fct Feuille

--- qwinto/src/Model/Entity/Feuille.php
+++ qwinto/src/Model/Entity/Feuille.php
@@ -11,20 +11,38 @@ use Cake\ORM\Entity;
 
 class Feuille extends Entity {
     
-    private $nombre = array(array());
+    private $nombres;
     private $nombreCroix;
     private $aJoue;
     private $ordre;
     
+    /* Retourne le score de la feuille */
     public function score() {
         
     }
     
+    /* Indique si une feuille est achevée : 2 lignes pleines ou 4 croix */
     public function fin() {
         
     }
     
+    /* Place la valeur n aux coordonnées (i,j) */
     public function placer($i, $j, $n) {
+        
+    }
+    
+    /* Indique si c'est une case (à cause du décalage des lignes) */
+    public function estCase($i, $j) {
+        
+    }
+    
+    /* Indique si le placement de n aux coordonnées (i,j) est valable */
+    public function valable($i, $j, $n) {
+        
+    }
+    
+    
+    public function lignePleine($l) {
         
     }
     
