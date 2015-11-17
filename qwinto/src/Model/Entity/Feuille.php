c4b2236308516dad713e33f32211dad495bb84b0
DecolleAnais <anais.decolle8@etu.univ-lorraine.fr>
2015-11-17 16:16:37 +0100

    ajout Entite Feuille

---
+++ qwinto/src/Model/Entity/Feuille.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Feuille d'un joueur
+ *
+ * @author decolle2u
+ */
+
+namespace qwinto\Model\Entity;
+use Cake\ORM\Entity;
+
+class Feuille extends Entity {
+    
+    private $nombre;
+    
+    public function score()
+    {
+        
+    }
+    
+}
