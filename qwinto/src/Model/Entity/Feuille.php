4d5397e56124de3e2ce2c7f750da3c25e03642f2
DecolleAnais <anais.decolle8@etu.univ-lorraine.fr>
2015-11-20 15:52:57 +0100

    blaaaaaaaaaaaaaaaaaaaaaa

--- qwinto/src/Model/Entity/Feuille.php
+++ qwinto/src/Model/Entity/Feuille.php
@@ -15,19 +15,21 @@ class Feuille extends Entity {
     private $nombreCroix;
     private $aJoue;
     private $ordre;
+    private $des;
     
     public function Feuille() {
-        $this->$nombres = array(
+        $this->nombres = array(
             array(-1,-1, 0,0,0,-1,0,0,0,0,0,0),
             array(-1,0,0,0,0,0, -1,0,0,0,0,-1),
             array(0,0,0,0,-1,0,0,0,0,0,-1,-1)
             );
-            $this->$nombreCroix = 0;
-            $this->$ajoue = FALSE;
-            $this->$ordre = \Cake\ORM\TableRegistry::get('Feuilles');
+        $this->des = array(0,0,0);
+        $this->nombreCroix = 0;
+        $this->ajoue = FALSE;
+        $this->ordre = \Cake\ORM\TableRegistry::get('Feuilles');
 
-            $this->$aJoue = FALSE;
-            $this->$ordre = -1;
+        $this->aJoue = FALSE;
+        $this->ordre = -1;
 
     }
     
@@ -35,13 +37,13 @@ class Feuille extends Entity {
     /* Retourne le score de la feuille */
     public function score() {
         $score = 0;
-        return $score - $this->$nombreCroix * 5;
+        return $score - $this->nombreCroix * 5;
     }
     
     /* Indique si une feuille est achevée : 2 lignes pleines ou 4 croix */
     public function fin() {
         /* Fin du jeu si on a 4 croix */
-        if($this->$nombreCroix == 4) {
+        if($this->nombreCroix == 4) {
             return true;
         }
         /* Calcul du nombre de lignes pleines */
@@ -64,9 +66,9 @@ class Feuille extends Entity {
         
     }
     
-    /* Indique si c'est une case (à cause du décalage des lignes) */
+    /* Indique si c'est une case (a cause du decalage des lignes) */
     public function estCase($i, $j) {
-        if($this->$nombre[i][j] != -1){
+        if($this->nombres[$i][$j] != -1){
             return TRUE;
         }
         else {
@@ -76,7 +78,13 @@ class Feuille extends Entity {
     
     /* Indique si le placement de n aux coordonnées (i,j) est valable */
     public function valable($i, $j, $n) {
-        
+        /* Verifications :                                     */
+        /* case aux coordonnees (i,j) est vide                 */
+        /* le de correspondant a la ligne a ete lance          */
+        /* les valeurs de la ligne sont dans l'ordre croissant */ 
+        if($this->nombres[$i][$j] == 0 && $this->des[$i] != 0) {
+            
+        }
     }
     
     /* Indique si la ligne numero i (en partant de 0) est pleine */
@@ -86,7 +94,7 @@ class Feuille extends Entity {
             case 0: 
                 /* premiere ligne */
                 for($num = 2;$num < 12;$num++) {
-                    if($this->$nombres[0][$num] == 0 && estCase(0, $num)) {
+                    if($this->nombres[0][$num] == 0 && estCase(0, $num)) {
                         $estPleine = false;
                     }
                 }
@@ -94,7 +102,7 @@ class Feuille extends Entity {
             case 1:
                 /* deuxieme ligne */
                 for($num = 1;$num < 11; $num++) {
-                    if($this->$nombres[1][$num] == 0 && estCase(1, $num)) {
+                    if($this->nombres[1][$num] == 0 && estCase(1, $num)) {
                         $estPleine = false;
                     }                   
                 }
@@ -102,7 +110,7 @@ class Feuille extends Entity {
             case 2:
                 /* troisieme ligne */
                 for($num = 0;$num < 10; $num++) {
-                    if($this->$nombres[2][$num] == 0 && estCase(2, $num)) {
+                    if($this->nombres[2][$num] == 0 && estCase(2, $num)) {
                         $estPleine = false;
                     }                   
                 }
@@ -119,10 +127,10 @@ class Feuille extends Entity {
     function scoreLigne1() {
         $score = 0;
         if(lignePleine(0)) {
-            $score = $this->$nombres[0][11];   
+            $score = $this->nombres[0][11];   
         }else {
             for($num = 2;$num < 12;$num++) {
-                if(estCase(0,$num) && $this->$nombres[0][$num] != 0) {
+                if(estCase(0,$num) && $this->nombres[0][$num] != 0) {
                     $score++;
                 }
             }
@@ -134,10 +142,10 @@ class Feuille extends Entity {
     function scoreLigne2() {
         $score = 0;
         if(lignePleine(1)) {
-            $score = $this->$nombres[1][10];   
+            $score = $this->nombres[1][10];   
         }else {
             for($num = 1;$num < 11;$num++) {
-                if(estCase(1,$num) && $this->$nombres[1][$num] != 0) {
+                if(estCase(1,$num) && $this->nombres[1][$num] != 0) {
                     $score++;
                 }
             }
@@ -149,10 +157,10 @@ class Feuille extends Entity {
     function scoreLigne3() {
         $score = 0;
         if(lignePleine(2)) {
-            $score = $this->$nombres[2][9];   
+            $score = $this->nombres[2][9];   
         }else {
             for($num = 0;$num < 10;$num++) {
-                if(estCase(2,$num) && $this->$nombres[2][$num] != 0) {
+                if(estCase(2,$num) && $this->nombres[2][$num] != 0) {
                     $score++;
                 }
             }
@@ -160,7 +168,17 @@ class Feuille extends Entity {
         return $score;
     }
     
-    
+    /* Choix des des a lancer, nombre de parametres variables       */
+    /* exemple : lancer le de 1 (rouge) et le de violet (3)         */
+    /* lancerDes(1, 3);                                             */
+    /* si erreur : verifier que la version de php est assez recente */
+    function getDes(...$numDe) {
+        $i = 0;
+        foreach($numDe as $de) {
+            $this->des[$i] = $de;
+            $i++;
+        }
+    }
     
     
 }
