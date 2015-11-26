79fc948d239f3258f17ef018994d0530d4b34303
Terrtia <or1994@hotmail.fr>
2015-11-26 10:56:41 +0100

    a

---
+++ qwinto/tests/Fixture/PartiesFixture.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * PartiesFixture
+ *
+ */
+class PartiesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'ID' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'TOUR' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'NUM_FEUILLES' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_indexes' => [
+            'NUM_FEUILLES' => ['type' => 'index', 'columns' => ['NUM_FEUILLES'], 'length' => []],
+            'ID' => ['type' => 'index', 'columns' => ['ID'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['ID', 'NUM_FEUILLES'], 'length' => []],
+            'parties_ibfk_1' => ['type' => 'foreign', 'columns' => ['NUM_FEUILLES'], 'references' => ['feuilles', 'ID'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'ID' => 1,
+            'TOUR' => 1,
+            'NUM_FEUILLES' => 1
+        ],
+    ];
+}
