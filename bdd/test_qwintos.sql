82f9b812f9d0dac27272b7a3fcaf1d6607a4dc08
Terrtia <or1994@hotmail.fr>
2015-11-29 15:31:51 +0100

    new bdd

--- bdd/test_qwintos.sql
+++ bdd/test_qwintos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 26 Novembre 2015 à 18:38
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client: localhost
+-- Généré le: Dim 29 Novembre 2015 à 14:39
+-- Version du serveur: 5.5.46-0ubuntu0.14.04.2
+-- Version de PHP: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `cake_qwintos`
+-- Base de données: `test_qwintos`
 --
 
 -- --------------------------------------------------------
@@ -28,25 +28,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `feuilles` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRES_ID` int(11) NOT NULL,
   `NOMBRES_CROIX` int(11) NOT NULL,
   `AJOUTER` tinyint(1) NOT NULL,
   `ORDRE` int(11) NOT NULL,
   `NUM_PARTY` int(11) NOT NULL,
   `USER_ID` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
-  KEY `NOMBRES_ID` (`NOMBRES_ID`),
   KEY `NUM_PARTY` (`NUM_PARTY`),
   KEY `ID_USER` (`USER_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `feuilles`
 --
 
-INSERT INTO `feuilles` (`ID`, `NOMBRES_ID`, `NOMBRES_CROIX`, `AJOUTER`, `ORDRE`, `NUM_PARTY`, `USER_ID`) VALUES
-(4, 2, 0, 0, 1, 1, 2),
-(5, 2, 0, 1, 1, 1, 2);
+INSERT INTO `feuilles` (`ID`, `NOMBRES_CROIX`, `AJOUTER`, `ORDRE`, `NUM_PARTY`, `USER_ID`) VALUES
+(7, 1, 0, 2, 4, 6);
 
 -- --------------------------------------------------------
 
@@ -55,22 +52,19 @@ INSERT INTO `feuilles` (`ID`, `NOMBRES_ID`, `NOMBRES_CROIX`, `AJOUTER`, `ORDRE`,
 --
 
 CREATE TABLE IF NOT EXISTS `nombres` (
-  `TABLE_ID` int(11) NOT NULL,
-  `NOMBRES_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `LIGNE` int(11) DEFAULT NULL,
-  `COLONE` int(11) DEFAULT NULL,
-  `VAL` int(11) NOT NULL,
-  PRIMARY KEY (`NOMBRES_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `FEUILLE_ID` int(11) NOT NULL,
+  `LIGNE` int(11) NOT NULL,
+  `COLONNE` int(11) NOT NULL,
+  `VAL` int(11) DEFAULT NULL,
+  PRIMARY KEY (`FEUILLE_ID`,`LIGNE`,`COLONNE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `nombres`
 --
 
-INSERT INTO `nombres` (`TABLE_ID`, `NOMBRES_ID`, `LIGNE`, `COLONE`, `VAL`) VALUES
-(0, 1, 3, 1, 0),
-(0, 2, 3, 1, 1),
-(0, 3, 2, 3, 10);
+INSERT INTO `nombres` (`FEUILLE_ID`, `LIGNE`, `COLONNE`, `VAL`) VALUES
+(7, 0, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -81,19 +75,16 @@ INSERT INTO `nombres` (`TABLE_ID`, `NOMBRES_ID`, `LIGNE`, `COLONE`, `VAL`) VALUE
 CREATE TABLE IF NOT EXISTS `parties` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TOUR` int(11) NOT NULL,
-  `NUM_FEUILLES` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `NUM_FEUILLES` (`NUM_FEUILLES`),
   KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `parties`
 --
 
-INSERT INTO `parties` (`ID`, `TOUR`, `NUM_FEUILLES`) VALUES
-(1, 1, NULL),
-(2, 1, NULL);
+INSERT INTO `parties` (`ID`, `TOUR`) VALUES
+(4, 1);
 
 -- --------------------------------------------------------
 
@@ -105,17 +96,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `LOGIN` varchar(10) NOT NULL,
   `PASSWORD` varchar(100) NOT NULL,
-  `NUM_FEUILLES` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `NUM_FEUILLES` (`NUM_FEUILLES`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `NUM_FEUILLES`) VALUES
-(2, 'axel', '1234', NULL);
+INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`) VALUES
+(6, 'test', 'secret');
 
 --
 -- Contraintes pour les tables exportées
@@ -126,20 +115,13 @@ INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `NUM_FEUILLES`) VALUES
 --
 ALTER TABLE `feuilles`
   ADD CONSTRAINT `feuilles_ibfk_1` FOREIGN KEY (`NUM_PARTY`) REFERENCES `parties` (`ID`),
-  ADD CONSTRAINT `feuilles_ibfk_2` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`ID`),
-  ADD CONSTRAINT `feuilles_ibfk_3` FOREIGN KEY (`NOMBRES_ID`) REFERENCES `nombres` (`NOMBRES_ID`);
+  ADD CONSTRAINT `feuilles_ibfk_2` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`ID`);
 
 --
--- Contraintes pour la table `parties`
+-- Contraintes pour la table `nombres`
 --
-ALTER TABLE `parties`
-  ADD CONSTRAINT `parties_ibfk_1` FOREIGN KEY (`NUM_FEUILLES`) REFERENCES `feuilles` (`ID`);
-
---
--- Contraintes pour la table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`NUM_FEUILLES`) REFERENCES `feuilles` (`ID`);
+ALTER TABLE `nombres`
+  ADD CONSTRAINT `nombres_ibfk_1` FOREIGN KEY (`FEUILLE_ID`) REFERENCES `feuilles` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
